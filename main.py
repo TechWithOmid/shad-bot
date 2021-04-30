ff1d7f472446fd2c76dd95435de02ac57647dddd
omid <omidmmadi@gmail.com>
2021-04-30 21:16:18 +0430

    add benner and some modyfication

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+"""
+Blog: TechWithOmid.ir
+Email: TechWithOmid.gmail.com
+
+feel free to use this script and report bug and add feature
+"""
 import os
 import time
+import pyfiglet
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # TODO
 # make form presntaion availble
-# add menu
-# build cli interface
+
 
 PATH = "/usr/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
@@ -15,13 +21,28 @@ driver.set_window_position(-10000,0)
 driver.get("https://web.shad.ir")
 
 
+def main():
+    """
+    Show when program start.
+    """
+    start_banner = pyfiglet.figlet_format("Shad Bot!!")
+    print(start_banner)
+    user_param = input("Press Enter to continue ")
+    if user_param == "":
+        login()
+    else:
+        pass
+
+
 def login():
     os.system('clear')
+    print(pyfiglet.figlet_format("Shad Bot!!")) # print banner
+
     user_phone_number = input('Enter Your Phone Number: ') # user phone number
     number_field = driver.find_element_by_xpath("/html//app-root/tab-login/div[@class='login_page_wrap']//form[@name='mySendCodeForm']//input[@name='phone_number']") # number input field in login page
     number_field.clear() # clear number field
     number_field.send_keys(user_phone_number) # input number to number field
-    
+
     driver.find_element_by_xpath("/html/body//app-root/tab-login/div[@class='login_page_wrap']//span[.='بعدی']").click()
     number_check = input('Are you sure (y/n): ')
     if number_check == 'y':
@@ -33,6 +54,9 @@ def login():
 
 
 def login_confirm():
+    os.system('clear')
+    print(pyfiglet.figlet_format("Shad Bot!!")) # print banner
+
     confirm_code = input('enter the code that we send: ') # Shad login confirm code
     confirm_field = driver.find_element_by_xpath("/html//app-root/tab-login/div[@class='login_page_wrap']//form[@name='myLoginForm']//input[@name='phone_code']") # the confirm code input field
     confirm_field.clear()
@@ -41,6 +65,9 @@ def login_confirm():
 
 
 def select_chat():
+    os.system('clear')
+    print(pyfiglet.figlet_format("Shad Bot!!")) # print banner
+
     chat_id = input('Enter chat id from top: ')
     chat_path = "/html//app-root//rb-chats/div[@class='im_dialogs_col_wrap noselect']/div[@class='im_dialogs_col']/div/div[@class='im_dialogs_scrollable_wrap nano-content']/ul[2]/li["+chat_id+"]//div[@class='im_dialog_peer']"
     driver.find_element_by_xpath(chat_path).click()
@@ -48,10 +75,13 @@ def select_chat():
 
 
 def send_present_msg():
+    os.system('clear')
+    print(pyfiglet.figlet_format("Shad Bot!!")) # print banner
+
     send_hour = input('Enter the hour you want to send message: ') # ask for time that user want to send message
     send_min = input('Enter the min you want to send message: ')
     user_message = input('Enter the message you want to send: ') # message user want to send
-    
+
     while True:
         current_time = datetime.now()
         current_hour = current_time.strftime('%H')
@@ -72,9 +102,10 @@ def send_present_msg():
             # driver.find_element_by_xpath("//html/body/div[1]/app-root/span/div[1]/div/div/app-tab-container/app-tab-view/div[2]/tab-conversation/div/div[3]/div/div/div/form/div[4]/button").click() # send message button path
 
             time.sleep(4)
+            os.system('clear')
+            print(pyfiglet.figlet_format("Shad Bot!!")) # print banner
             print('Message send.')
             select_chat()
-    print('send_present_msg done')
-            
 
-login()
+
+main()
