57da4ba852ad288493d74b0dc5da343c2079ca4f
omid <omidmmadi@gmail.com>
2021-05-03 18:05:19 +0430

    multiple class selection available

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 # TODO
-# make form presentation available
-# make multiple class selection available
+# make multiple class selection available => Done!
+# detect the os for using clear and cls
+# define check time
 
 # browser setting
 PATH = "/usr/bin/chromedriver"
@@ -31,6 +32,7 @@ def main():
     """
     Script start point
     """
+    os.system('clear')
     start_banner = pyfiglet.figlet_format("Shad Bot!!")
     print(start_banner)
     user_param = input("Press Enter to continue ")
@@ -82,21 +84,45 @@ def select_chat():
     os.system('clear')
     print(pyfiglet.figlet_format("Shad Bot!!"))  # print banner
 
-    first_chat_id = input('Enter chat id from top: ')
+    first_chat_id = input('Enter 1st chat id from top: ')
+    first_class_time = input(
+        'Enter the 1st class time(e.g. 12:10): ').split(':')  # time that user want to send message
+
+    second_chat_id = input('Enter 2nd chat id from top: ')
+    second_class_time = input(
+        'Enter the 2nd class time(e.g. 12:30): ').split(':')  # used split to save the hour and min in a list
+
+    third_chat_id = input('Enter 3rd chat id from top: ')
+    third_class_time = input('Enter the 3rd class time(e.g. 12:30): ').split(':')
+
+    fourth_chat_id = input('Enter 4th chat id from top: ')
+    fourth_class_time = input('Enter the 4th class time(e.g. 12:30): ').split(':')
+
     first_chat_path = "/html//app-root//rb-chats/div[@class='im_dialogs_col_wrap noselect']/div[" \
                       "@class='im_dialogs_col']/div/div[@class='im_dialogs_scrollable_wrap nano-content']/ul[2]/li[" \
                       "" + first_chat_id + "]//div[@class='im_dialog_peer'] "
 
-    send_present_msg(first_chat_path)
+    second_chat_path = "/html//app-root//rb-chats/div[@class='im_dialogs_col_wrap noselect']/div[" \
+                       "@class='im_dialogs_col']/div/div[@class='im_dialogs_scrollable_wrap nano-content']/ul[2]/li[" \
+                       "" + second_chat_id + "]//div[@class='im_dialog_peer'] "
+
+    third_chat_path = "/html//app-root//rb-chats/div[@class='im_dialogs_col_wrap noselect']/div[" \
+                       "@class='im_dialogs_col']/div/div[@class='im_dialogs_scrollable_wrap nano-content']/ul[2]/li[" \
+                       "" + third_chat_id + "]//div[@class='im_dialog_peer'] "
+
+    fourth_chat_path = "/html//app-root//rb-chats/div[@class='im_dialogs_col_wrap noselect']/div[" \
+                       "@class='im_dialogs_col']/div/div[@class='im_dialogs_scrollable_wrap nano-content']/ul[2]/li[" \
+                       "" + fourth_chat_id + "]//div[@class='im_dialog_peer'] "
+    send_present_msg(first_chat_path, second_chat_path, first_class_time, second_class_time, third_class_time, fourth_class_time, third_chat_path, fourth_chat_path)
 
 
-def send_present_msg(first_chat_path):
+def send_present_msg(first_chat_path, second_chat_path, first_class_time, second_class_time, third_class_time, fourth_class_time, third_chat_path, fourth_chat_path):
     os.system('clear')
     print(pyfiglet.figlet_format("Shad Bot!!"))  # print banner
 
-    send_hour = input('Enter the hour you want to send message: ')  # ask for time that user want to send message
-    send_min = input('Enter the min you want to send message: ')
     user_message = input('Enter the message you want to send: ')  # message user want to send
+    message_box_path = "/html/body/div[1]/app-root/span/div[1]/div/div/app-tab-container/app-tab-view/div[" \
+                       "2]/tab-conversation/div/div[3]/div/div/div/form/div[3]/div[5]"  # textarea path for input text
 
     while True:
         current_time = datetime.now()
@@ -109,23 +135,37 @@ def send_present_msg(first_chat_path):
         time.sleep(0.5)
         os.system('clear')
 
-        if current_hour == send_hour and current_min == send_min:
+        if first_class_time[0] == current_hour and first_class_time[1] == current_min:
             driver.find_element_by_xpath(first_chat_path).click()
-
-            message_box = driver.find_element_by_xpath("/html/body/div[1]/app-root/span/div["
-                                                       "1]/div/div/app-tab-container/app-tab-view/div["
-                                                       "2]/tab-conversation/div/div[3]/div/div/div/form/div[3]/div["
-                                                       "5]")  # textarea path for input text
-
+            message_box = driver.find_element_by_xpath(message_box_path)
             message_box.clear()
             message_box.send_keys(user_message)
             message_box.send_keys(Keys.ENTER)
+            time.sleep(60)
 
-            time.sleep(4)
-            os.system('clear')
-            print(pyfiglet.figlet_format("Shad Bot!!"))  # print banner
-            print('Message send.')
-            select_chat()
+        if second_class_time[0] == current_hour and second_class_time[1] == current_min:
+            driver.find_element_by_xpath(second_chat_path).click()
+            message_box = driver.find_element_by_xpath(message_box_path)
+            message_box.clear()
+            message_box.send_keys(user_message)
+            message_box.send_keys(Keys.ENTER)
+            time.sleep(60)
+
+        if third_class_time[0] == current_hour and third_class_time[1] == current_min:
+            driver.find_element_by_xpath(third_chat_path).click()
+            message_box = driver.find_element_by_xpath(message_box_path)
+            message_box.clear()
+            message_box.send_keys(user_message)
+            message_box.send_keys(Keys.ENTER)
+            time.sleep(60)
+
+        if fourth_class_time[0] == current_hour and fourth_class_time[1] == current_min:
+            driver.find_element_by_xpath(fourth_chat_path).click()
+            message_box = driver.find_element_by_xpath(message_box_path)
+            message_box.clear()
+            message_box.send_keys(user_message)
+            message_box.send_keys(Keys.ENTER)
+            time.sleep(60)
 
 
 main()
